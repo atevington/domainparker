2101828333ca50fe39cf5fbbf51f420cb139c9e6
andrewtevington <andrew.tevington@gmail.com>
2017-08-04 22:43:31 -0400

    host adjustments

--- controllers/basePage.js
+++ controllers/basePage.js
@@ -5,54 +5,54 @@ var getWikiTextAndUrl = require("../utils/wiki.js");
 var getRssFeed = require("../utils/rss.js");
 
 var getPageContent = function(config, fail, success) {
-	if (config.hostNames[config.hostKey]) {
-		if (pageCache.get(config.hostKey)) {
-			success(pageCache.get(config.hostKey));
-		} else {
-			var pageConfig = Object.assign({}, config.hostNames[config.hostKey]);
-
-			getWikiTextAndUrl(pageConfig.wiki.articleTitle, pageConfig.wiki.sectionTitle, fail, function(wikiText, wikiUrl) {
-				getRssFeed(pageConfig.news.rssFeed, fail, function(newsEntries) {
-					pageConfig.hostName = config.hostKey;
-					pageConfig.canoncialhostName = "http://" + config.hostKey + "/";
-					pageConfig.canoncialhostNameEncoded = encodeURIComponent("http://" + config.hostKey + "/");
-					
-					pageConfig.wiki.articleContent = wikiText;
-					pageConfig.wiki.articleUrl = wikiUrl;
-					pageConfig.news.entries = newsEntries;
-
-					pageCache.set(
-						config.hostKey,
-						mustache.render(config.indexFileText, pageConfig),
-						config.expireSeconds
-					);
-					
-					success(pageCache.get(config.hostKey));
-				});
-			});
-		}
+	if (pageCache.get(config.hostKey)) {
+		success(pageCache.get(config.hostKey));
 	} else {
-		fail();
+		var pageConfig = Object.assign({}, config.hostNames[config.hostKey]);
+
+		getWikiTextAndUrl(pageConfig.wiki.articleTitle, pageConfig.wiki.sectionTitle, fail, function(wikiText, wikiUrl) {
+			getRssFeed(pageConfig.news.rssFeed, fail, function(newsEntries) {
+				pageConfig.hostName = config.hostKey;
+				pageConfig.canoncialhostName = "http://" + config.hostKey + "/";
+				pageConfig.canoncialhostNameEncoded = encodeURIComponent("http://" + config.hostKey + "/");
+					
+				pageConfig.wiki.articleContent = wikiText;
+				pageConfig.wiki.articleUrl = wikiUrl;
+				pageConfig.news.entries = newsEntries;
+
+				pageCache.set(
+					config.hostKey,
+					mustache.render(config.indexFileText, pageConfig),
+					config.expireSeconds
+				);
+				
+				success(pageCache.get(config.hostKey));
+			});
+		});
 	}
 };
 
-module.exports = function(port, hostNames, indexFileText, expireSeconds) {
+module.exports = function(hostNames, indexFileText, expireSeconds) {
 	return function(req, res, next) {
-		var config = {
-			hostNames: hostNames,
-			hostKey: req.headers.host.replace(".localhost:" + port, ""),
-			indexFileText: indexFileText,
-			expireSeconds: expireSeconds
-		};
-		
-		var fail = function() {
-			res.status(500).send({message: "An unknown error has occurred."});
-		};
-		
-		var success = function(pageContent) {
-			res.send(pageContent);
-		};
+		if (hostNames[req.params.host]) {
+			var config = {
+				hostNames: hostNames,
+				hostKey: req.params.host,
+				indexFileText: indexFileText,
+				expireSeconds: expireSeconds
+			};
+			
+			var fail = function() {
+				res.status(500).send({message: "An unknown error has occurred."});
+			};
+			
+			var success = function(pageContent) {
+				res.send(pageContent);
+			};
 
-		getPageContent(config, fail, success);
+			getPageContent(config, fail, success);	
+		} else {
+			next();
+		}
 	};
 };
--- index.js
+++ index.js
@@ -9,19 +9,18 @@ var port = process.env.PORT || 3000;
 var cacheExpireSeconds = 10800; // 3 hours
 
 function setUpRoutes(indexFileText, hostNames) {
-	var basePageController = controllers.basePage(port, hostNames, indexFileText, cacheExpireSeconds);
+	var basePageController = controllers.basePage(hostNames, indexFileText, cacheExpireSeconds);
 	
-	app.use("/", function(req, res, next) {
+	app.use("/:host", function(req, res, next) {
 		if (req.path === "/") {
 			basePageController(req, res, next);
 		} else {
 			next();
 		}
 	});
-	
-	app.use("/index.*", basePageController);
-	
-	app.use(express.static("public"));
+
+	app.use("/:host/index.*", basePageController);
+	app.use("/:host", express.static("public"));
 
 	app.listen(port, function() {
 		console.log("Listening on port " + port + ".");
